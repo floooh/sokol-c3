c18d8abccfc0faa63f3e610f48f89ea599ec0b7e
Radek Micek <radek.micek@gmail.com>
2024-11-15 18:47:37 +0100

    Add bash script to build all examples

---
+++ build-examples.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+script_dir=$(cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd)
+build_dir="$script_dir/build"
+
+mkdir -p "$build_dir"
+cd "$build_dir"
+
+c3c compile --libdir .. --lib sokol ../examples/clear/*.c3 -o clear
