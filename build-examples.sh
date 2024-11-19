3cfa28cc05017a2bcc120b775a7459ba050179d5
Radek Micek <radek.micek@gmail.com>
2024-11-19 12:54:10 +0100

    Port bufferoffsets example from sokol-odin

--- build-examples.sh
+++ build-examples.sh
@@ -9,3 +9,4 @@ cd "$build_dir"
 c3c compile --libdir .. --lib sokol ../examples/clear/*.c3 -o clear
 c3c compile --libdir .. --lib sokol ../examples/triangle/*.c3 -o triangle
 c3c compile --libdir .. --lib sokol ../examples/quad/*.c3 -o quad
+c3c compile --libdir .. --lib sokol ../examples/bufferoffsets/*.c3 -o bufferoffsets
